31806777ae86a16a6c2fe5a9c19fea62d532933d
sarowarhosen03 <md.sarowar.hosse@gmail.com>
2025-08-11 20:31:05 +0600

    refactor: enhance project section with isHome prop for better display logic

--- components/pages/Index.tsx
+++ components/pages/Index.tsx
@@ -5,28 +5,29 @@ import { SocialLink } from "@/app/dashboard/profile/action";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import { PersonalInfo, Project, Skill } from "@/lib/generated/prisma";
-import Navigation from "../Navigation";
 
 const Index = ({ data }: { data: [PersonalInfo, Skill[], Project[]] }) => {
   const [personalInfo, skill, projects] = data
   return (
-  
-      <main>
-        <section id="home">
-          <Hero name={personalInfo.name} bio={personalInfo.description} socialLinks={personalInfo.socialLinks as SocialLink} />
-        </section>
-        <section id="skills" >
-          <Skills skill={skill} />
-        </section>
-        <section id="projects">
-          <Projects projects={projects} />
-        </section>
-        <section id="contact">
-          <Contact
-            personalInfo={personalInfo}
-          />
-        </section>
-      </main>
+
+    <main>
+      <section id="home">
+        <Hero name={personalInfo.name} bio={personalInfo.description} socialLinks={personalInfo.socialLinks as SocialLink} />
+      </section>
+      <section id="skills" >
+        <Skills skill={skill} />
+      </section>
+      <section id="projects">
+        <Projects
+          isHome={projects.length > 3}
+          projects={projects} />
+      </section>
+      <section id="contact">
+        <Contact
+          personalInfo={personalInfo}
+        />
+      </section>
+    </main>
 
   );
 };
