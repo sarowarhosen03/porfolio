6d1ddac65d377c15430ab90adf491502c92f4cff
sarowarhosen03 <md.sarowar.hosse@gmail.com>
2025-08-11 20:01:42 +0600

    feat: update revalidation paths in revalidateAllRoute function for improved cache management

--- app/dashboard/action.ts
+++ app/dashboard/action.ts
@@ -3,6 +3,10 @@
 import { revalidatePath } from "next/cache";
 
 export default async function revalidateAllRoute() {
-  revalidatePath("/", "layout");
+  revalidatePath("/dashboard/projects");
+  revalidatePath("/dashboard");
+  revalidatePath("/");
+  revalidatePath("/(main)/project/[projectId]", "page");
+  revalidatePath("/dashboard/skills");
   return { status: "ok" };
 }
