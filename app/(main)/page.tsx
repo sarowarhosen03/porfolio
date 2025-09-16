22d025bb6393ca67e8d01ec5d4577e8e24fa90b9
sarowarhosen03 <md.sarowar.hosse@gmail.com>
2025-09-17 01:18:15 +0600

    feat(layout): update metadata to use personalInfo for dynamic title and Open Graph/Twitter data; clean up unused imports in Home page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,6 @@
 import Index from "@/components/pages/Index";
 import resolvePromise from "@/lib/resolvePromise";
 import { dbClient } from "@/prismaClient";
-import { personalInfo } from "@/utils/data";
 
 export default async function Home() {
   const [data, error] = await resolvePromise(
@@ -18,8 +17,6 @@ export default async function Home() {
   if (error) return <div>Error happened</div>;
   if (!data || !data[0]) return <div>No personal info found</div>;
   const [projects] = data;
-
-  console.log(personalInfo);
 
   return <Index data={[projects]} />;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import PageTransition from "@/components/site/PageTransition";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { personalInfo } from "@/utils/data";
 import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -22,8 +23,27 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Samovar Hessian",
+  title: personalInfo.name,
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL!),
   description: "Building elegant solutions, one line at a time.",
+  openGraph: {
+    title: personalInfo.name,
+    description: "Building elegant solutions, one line at a time.",
+    images: [
+      {
+        url: "/profile.png",
+        width: 1200,
+        height: 630,
+        alt: "Profile image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Samovar Hessian",
+    description: "Building elegant solutions, one line at a time.",
+    images: ["/profile.png"],
+  },
 };
 
 export default async function RootLayout({
@@ -36,7 +56,7 @@ export default async function RootLayout({
 
   return (
 
-    < html lang="en" >
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
